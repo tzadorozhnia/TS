f387d6871bc80a33869adb2b365f00e186f9a21c
tzadorozhnia <t.zadorozhnia2025@gmail.com>
2026-08-31 17:03:29 +0100

    hw 9-11

--- lessons/HomeWorksAnalysis/hw7/hw7_1/script.js
+++ lessons/HomeWorksAnalysis/hw7/hw7_1/script.js
@@ -1,92 +1,41 @@
-// #XjJuucOMR0
-//
-// – Створити функцію конструктор для об’єктів User з полями id, name, surname , email, phone
-//
-// створити пустий масив, наповнити його 10 об’єктами new User(….)
-//@ts-ignore
-// class User{
-//     private _id:number;
-//     private _name:string;
-//     private _surname:string;
-//     private _email:string;
-//     private _phone:string;
-//
-//     constructor(id:number, name:string, surname:string , email:string, phone:string){
-//         this._id = id;
-//         this._name = name;
-//         this._surname = surname;
-//         this._email = email;
-//         this._phone = phone;
-//
-//     }
-//
-//
-//     get id(): number {
-//         return this._id;
-//     }
-//
-//     set id(value: number) {
-//         this._id = value;
-//     }
-//
-//     get name(): string {
-//         return this._name;
-//     }
-//
-//     set name(value: string) {
-//         this._name = value;
-//     }
-//
-//     get surname(): string {
-//         return this._surname;
-//     }
-//
-//     set surname(value: string) {
-//         this._surname = value;
-//     }
-//
-//     get email(): string {
-//         return this._email;
-//     }
-//
-//     set email(value: string) {
-//         this._email = value;
-//     }
-//
-//     get phone(): string {
-//         return this._phone;
-//     }
-//
-//     set phone(value: string) {
-//         this._phone = value;
-//     }
-// }
-class User {
-    id;
-    name;
-    surname;
-    email;
-    phone;
-    constructor(id, name, surname, email, phone) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
+// @ts-ignore
+class User7 {
+    _id;
+    _name;
+    _surname;
+    _phone;
+    constructor(_id, _name, _surname, _phone) {
+        this._id = _id;
+        this._name = _name;
+        this._surname = _surname;
+        this._phone = _phone;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
     }
 }
+let user7 = new User7(1, 'asd', 'asd', 'asd');
+console.log(user7.id);
+user7.id = 123;
+// @ts-ignore
+const user = new User7(1, 'kokos', 'asdqwe', '+964657983');
+console.log(user);
+// @ts-ignore
 let users = [
-    new User(1, "Kait", "Winslet", "winslet@gmail.com", "067454555455"),
-    new User(2, "John", "Smith", "smith@gmail.com", "067111111111"),
-    new User(3, "Anna", "Brown", "brown@gmail.com", "067222222222"),
-    new User(4, "Peter", "Wilson", "wilson@gmail.com", "067333333333"),
-    new User(5, "Emma", "Taylor", "taylor@gmail.com", "067444444444"),
-    new User(6, "Michael", "Davis", "davis@gmail.com", "067555555555"),
-    new User(7, "Sarah", "Miller", "miller@gmail.com", "067666666666"),
-    new User(8, "David", "Anderson", "anderson@gmail.com", "067777777777"),
-    new User(9, "Laura", "Thomas", "thomas@gmail.com", "067888888888"),
-    new User(10, "James", "Moore", "moore@gmail.com", "067999999999")
+    new User7(1, 'kokos', 'asdqwe', '+964657983'),
+    new User7(1, 'kokos', 'asdqwe', '+964657983'),
+    new User7(1, 'kokos', 'asdqwe', '+964657983'),
+    new User7(1, 'kokos', 'asdqwe', '+964657983'),
+    new User7(1, 'kokos', 'asdqwe', '+964657983'),
+    new User7(1, 'kokos', 'asdqwe', '+964657983'),
+    new User7(1, 'kokos', 'asdqwe', '+964657983')
 ];
 console.log(users);
+// @ts-ignore
+const filterUsers = users.filter((user) => user.getId() % 2 === 0);
+console.log(filterUsers);
 export {};
 //# sourceMappingURL=script.js.map
--- lessons/HomeWorksAnalysis/hw7/hw7_2/script.js
+++ lessons/HomeWorksAnalysis/hw7/hw7_2/script.js
@@ -1,39 +1,18 @@
-// #2ikXsE2WiKZ
+//2ikXsE2WiKZ
+// - Взяти масив з  User[] з попереднього завдання (XjJuucOMR0), та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+// @ts-ignore
+// let users: User7[] = [
+//     new User7(1, 'kokos', 'asdqwe', '+964657983'),
+//     new User7(1, 'kokos', 'asdqwe', '+964657983'),
+//     new User7(1, 'kokos', 'asdqwe', '+964657983'),
+//     new User7(1, 'kokos', 'asdqwe', '+964657983'),
+//     new User7(1, 'kokos', 'asdqwe', '+964657983'),
+//     new User7(1, 'kokos', 'asdqwe', '+964657983'),
+//     new User7(1, 'kokos', 'asdqwe', '+964657983')
+// ];
 //
-// – Взяти масив з  User[] з попереднього завдання, та відфільтрувати, залишивши тільки об’єкти з парними id (filter)
-class User2 {
-    _id;
-    name;
-    surname;
-    email;
-    phone;
-    constructor(_id, name, surname, email, phone) {
-        this._id = _id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
-    }
-    get id() {
-        return this._id;
-    }
-    set id(value) {
-        this._id = value;
-    }
-}
-let users = [
-    new User2(1, "Kait", "Winslet", "winslet@gmail.com", "067454555455"),
-    new User2(2, "John", "Smith", "smith@gmail.com", "067111111111"),
-    new User2(3, "Anna", "Brown", "brown@gmail.com", "067222222222"),
-    new User2(4, "Peter", "Wilson", "wilson@gmail.com", "067333333333"),
-    new User2(5, "Emma", "Taylor", "taylor@gmail.com", "067444444444"),
-    new User2(6, "Michael", "Davis", "davis@gmail.com", "067555555555"),
-    new User2(7, "Sarah", "Miller", "miller@gmail.com", "067666666666"),
-    new User2(8, "David", "Anderson", "anderson@gmail.com", "067777777777"),
-    new User2(9, "Laura", "Thomas", "thomas@gmail.com", "067888888888"),
-    new User2(10, "James", "Moore", "moore@gmail.com", "067999999999")
-];
-let usFilter = users.filter((user) => user.id % 2 === 0);
-console.log(usFilter);
+//
+const filterUsers = users.filter((user) => user.id % 2 === 0);
 export {};
+// console.log(filterUsers);
 //# sourceMappingURL=script.js.map
